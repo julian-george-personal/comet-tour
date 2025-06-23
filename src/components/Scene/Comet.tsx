b7cc3f879164018ebb546d544660a99603ee8b71
Julian George <juliancgeorge@gmail.com>
2025-06-22 20:08:25 -0400

    ci and low res alt

--- src/components/Scene/Comet.tsx
+++ src/components/Scene/Comet.tsx
@@ -1,4 +1,4 @@
-import { FC, Ref, useMemo } from "react";
+import { FC, Ref, Suspense, useMemo } from "react";
 import { useLoader } from "@react-three/fiber";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { Euler, Mesh, TextureLoader } from "three";
@@ -12,17 +12,15 @@ interface CometProps {
   setLoaded: (newValue: boolean) => void;
 }
 
-const Comet: FC<CometProps> = ({ meshRef, setLoaded }) => {
+const CometMesh: FC<CometProps> = ({ meshRef, setLoaded }) => {
   const initialRotation = useMemo(() => new Euler(0, 0, -AXIAL_TILT), []);
   const cometObj = useLoader(
     OBJLoader,
-    // "https://comet-obj.s3.us-east-2.amazonaws.com/comet_67p.obj"
-    "./comet_67p.obj"
+    "https://comet-obj.s3.us-east-2.amazonaws.com/comet_67p.obj"
   );
   const cometMap = useLoader(TextureLoader, "./comet_texture.jpg");
   const geometry = useMemo(() => {
     let g;
-
     cometObj.traverse((c) => {
       if (c.type === "Mesh") {
         g = (c as Mesh).geometry;
@@ -31,15 +29,51 @@ const Comet: FC<CometProps> = ({ meshRef, setLoaded }) => {
     });
     return g;
   }, [cometObj, setLoaded]);
+
   return (
     <mesh
       geometry={geometry}
       rotation={initialRotation}
       ref={meshRef}
-      // castShadow
     >
       <meshPhongMaterial map={cometMap} />
     </mesh>
+  );
+};
+
+const LowResCometMesh: FC<CometProps> = ({ meshRef, setLoaded }) => {
+  const initialRotation = useMemo(() => new Euler(0, 0, -AXIAL_TILT), []);
+  const cometObj = useLoader(
+    OBJLoader,
+    "./comet_67p_mini.obj"
+  );
+  const cometMap = useLoader(TextureLoader, "./comet_texture.jpg");
+  const geometry = useMemo(() => {
+    let g;
+    cometObj.traverse((c) => {
+      if (c.type === "Mesh") {
+        g = (c as Mesh).geometry;
+      }
+    });
+    return g;
+  }, [cometObj]);
+
+  return (
+    <mesh
+      geometry={geometry}
+      rotation={initialRotation}
+      ref={meshRef}
+    >
+      <meshPhongMaterial map={cometMap} />
+    </mesh>
+  );
+};
+
+const Comet: FC<CometProps> = (props) => {
+  return (
+    <Suspense fallback={<LowResCometMesh {...props} />}>
+      <CometMesh {...props} />
+    </Suspense>
   );
 };
 
